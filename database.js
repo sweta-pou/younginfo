10682f199594a667e34478361e4dafa305db75cf
Sweta <sweta.poudel26@gmail.com>
2021-05-16 17:24:52 +0545

    .env check

--- database.js
+++ database.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
 
 const pg = require('pg');
- var client = new pg.Client({connectionString:process.env.DATABASE_URL,ssl: { rejectUnauthorized: false }})
+if(process.env.CHECK==='production')
+{
+  var client = new pg.Client({connectionString:process.env.DATABASE_URL
+    ,ssl: { rejectUnauthorized: false }
+  })
+}
+else
+{
+  var client = new pg.Client({connectionString:process.env.DATABASE_URL
+  })
+}
+ 
  client.connect();
 
   
